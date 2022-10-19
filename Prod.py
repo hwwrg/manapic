ffec3e60b14996eec9fa9dabbf48a921b3458f2b
hwwrg <86141485+hwwrg@users.noreply.github.com>
2022-10-19 10:42:53 +0200

    add photos

--- Prod.py
+++ Prod.py
@@ -1,10 +1,10 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-img = Image.open('050.JPG')
+img = Image.open('Capture001.png')
 info = img._getexif()
-print(info[36867])
+print(info)
 
-for k, v in info.items():
-    nice = TAGS.get(k, k)
-    # print('%s (%s) = %s' % (nice, k, v))
+# for k, v in info.items():
+#     nice = TAGS.get(k, k)
+#     # print('%s (%s) = %s' % (nice, k, v))
